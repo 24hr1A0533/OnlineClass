a1e9689bcc50597d51d2b26ed81376e825b988f1
24hr1A0533 <kingsofmcg@gmail.com>
2026-08-31 22:12:17 +0530

    ll

---
+++ dom practise3/script.js
@@ -0,0 +1,19 @@
+const title=document.getElementById("title");
+console.log(title);
+title.textContent="dom is intreasting";
+
+const heading=document.getElementById("heading");
+console.log(heading);
+heading.textContent="I LOVE JAVA SCRIPT";
+
+const message=document.getElementById("message");
+message.textContent="I am learning dom";
+
+const title2=document.getElementById("title2");
+title2.textContent="My Aswesome Website";
+
+const description=document.getElementById("description");
+description.textContent="I am learning java script Dom";
+
+const btn=document.getElementById("btn");
+btn.textContent="Start Learning";
--- dom/script.js
+++ dom/script.js
@@ -1,19 +1,7 @@
-const title=document.getElementById("title");
-console.log(title);
-title.textContent="dom is intreasting";
+const ll=document.querySelector("#ll");
+ll.textContent="haven";
+const lk=document.querySelector(".lk");
+lk.textContent="loss";
 
-const heading=document.getElementById("heading");
-console.log(heading);
-heading.textContent="I LOVE JAVA SCRIPT";
-
-const message=document.getElementById("message");
-message.textContent="I am learning dom";
-
-const title2=document.getElementById("title2");
-title2.textContent="My Aswesome Website";
-
-const description=document.getElementById("description");
-description.textContent="I am learning java script Dom";
-
-const btn=document.getElementById("btn");
-btn.textContent="Start Learning";
+const kishore=document.querySelector("h1");
+setTimeout(()=>kishore.textContent="wowwwwww",5000)
