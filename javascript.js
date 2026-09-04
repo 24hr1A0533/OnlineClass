6e901097696e4b394ac8dd2dbdc5588972afc640
24hr1A0533 <kingsofmcg@gmail.com>
2026-09-05 01:09:35 +0530

    ll

---
+++ javascript.js
@@ -0,0 +1,13 @@
+// VARIABLES 
+let StudentName=`kishore royal`;
+let StudentAge=19;
+let StudentBranch=`CSE`;
+let StudentCollage=`Mother thresa instuten of engineering and technology`;
+let StudentMarks=99.9;
+console.log(StudentName);
+console.log(StudentAge);
+console.log(StudentBranch);
+console.log(StudentCollage);
+console.log(StudentMarks);
+StudentMarks=100;
+console.log(StudentMarks);
