bfac7090b0498422eb6026489862c202a97a0a25
24hr1A0533 <kingsofmcg@gmail.com>
2026-09-05 22:55:55 +0530

    ll

--- javascript.js
+++ javascript.js
@@ -11,3 +11,17 @@ console.log(StudentCollage);
 console.log(StudentMarks);
 StudentMarks=100;
 console.log(StudentMarks);
+// arry methods 
+let arr=[1,2,3,4,5,6,7,8,9,10];
+let double=arr.map(value=>value*2);
+console.log(double);
+double.forEach((value,index)=>console.log(index+" "+value));
+arr.forEach((nums)=>console.log(nums));
+
+let result=[];
+const names=["kishore","anil","damu","lokanath"];
+names.forEach((name)=>result.push(name));
+console.log(result);
+// map
+let results=names.map((name)=>name);
+console.log(results);
